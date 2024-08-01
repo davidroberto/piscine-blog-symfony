16514ea1c5b520caf9178a563aed25cc53056e73
David Robert <contact@david-robert.fr>
2024-08-01 16:29:31 +0200

    show article and list articles

---
+++ src/Controller/Public/ArticleController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Public;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+
+    #[Route('/articles', 'list_articles')]
+    public function listArticles(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('public/page/article/list_articles.html.twig', [
+           'articles' => $articles
+        ]);
+    }
+
+
+    #[Route('/articles/{id}', 'show_article')]
+    public function showArticle(int $id, ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->find($id);
+
+        if (!$article || !$article->isPublished()) {
+            $html404 = $this->renderView('public/page/404.html.twig');
+            return new Response($html404, 404);
+        }
+
+        return $this->render('public/page/article/show_article.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+}
