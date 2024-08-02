9aa7f30cc48c00f4924e879c02e4b2db8768e887
David Robert <contact@david-robert.fr>
2024-08-02 14:42:59 +0200

    delete with 404 / error page + message flash

--- src/Controller/Admin/AdminArticleController.php
+++ src/Controller/Admin/AdminArticleController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,33 @@ class AdminArticleController extends AbstractController
         return $this->render('admin/page/article/list_articles.html.twig', [
            'articles' => $articles
         ]);
+    }
+
+    #[Route('/admin/articles/delete/{id}', 'admin_delete_article')]
+    public function deleteArticle(int $id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
+    {
+        $article = $articleRepository->find($id);
+
+        if (!$article) {
+            $html404 = $this->renderView('admin/page/404.html.twig');
+            return new Response($html404, 404);
+        }
+
+        try {
+            $entityManager->remove($article);
+            $entityManager->flush();
+
+            // permet d'enregistrer un message dans la session de PHP
+            // ce message sera affiché grâce à twig sur la prochaine page
+            $this->addFlash('success', 'Article bien supprimé !');
+
+        } catch(\Exception $exception){
+            return $this->renderView('admin/page/error.html.twig', [
+                'errorMessage' => $exception->getMessage()
+            ]);
+        }
+
+        return $this->redirectToRoute('admin_list_articles');
     }
 
 
